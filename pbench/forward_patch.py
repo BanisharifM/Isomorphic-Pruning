8c78118e3a10b1f9f3c994078b2ecc3630f471ac
BanisharifM <msharif@iastate.edu>
2025-07-10 19:39:34 -0500

    update the forward_patch.py to fix the mismatch new Timm version with this scritp

--- pbench/forward_patch.py
+++ pbench/forward_patch.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 def patch_timm_forward():
     
     @patch_to(timm.models.vision_transformer.Attention)
-    def forward(self, x):
+    def forward(self, x, attn_mask=None):   # <-- Add attn_mask
         """https://github.com/huggingface/pytorch-image-models/blob/054c763fcaa7d241564439ae05fbe919ed85e614/timm/models/vision_transformer.py#L79"""
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
@@ -16,15 +16,20 @@ def patch_timm_forward():
             x = F.scaled_dot_product_attention(
                 q, k, v,
                 dropout_p=self.attn_drop.p,
+                attn_mask=attn_mask    # Optionally pass if you want
             )
         else:
             q = q * self.scale
             attn = q @ k.transpose(-2, -1)
+
+            # If attn_mask is used and not None
+            if attn_mask is not None:
+                attn = attn + attn_mask
+
             attn = attn.softmax(dim=-1)
             attn = self.attn_drop(attn)
             x = attn @ v
 
-        # original implementation: x = x.transpose(1, 2).reshape(B, N, C), this force output dim to be C
         x = x.transpose(1, 2).reshape(B, N, -1) 
         x = self.proj(x)
         x = self.proj_drop(x)
